bd7196ec58e8077a2818328c41b81368c2c76536
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-12-09 02:42:29 -0300

    add-connecting-db-loading-models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import Student from './app/models/Student';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Vida!' });
+routes.get('/', async (req, res) => {
+  const student = await Student.create({
+    name: 'Bruno César Angst',
+    email: 'b@c.a',
+    age: 36,
+    weight: 95.201,
+    height: 1.74
+  });
+
+  return res.json(student);
 });
 
 export default routes;
